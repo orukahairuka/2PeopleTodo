d11ef2aec30acb8f258eb6f64169cb2fa2949b75
irukahaoruka <erika03301109@gmail.com>
2024-08-23 16:28:53 +0900

    first

---
+++ 2PeopleTodo/2PeopleTodo/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2PeopleTodo
+//
+//  Created by 櫻井絵理香 on 2024/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2PeopleTodo/_PeopleTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  _PeopleTodoApp.swift
+//  2PeopleTodo
+//
+//  Created by 櫻井絵理香 on 2024/08/23.
+//
+
+import SwiftUI
+
+@main
+struct _PeopleTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
