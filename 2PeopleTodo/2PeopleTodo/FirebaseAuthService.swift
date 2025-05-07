ac0a61930b4171e0f1588d49fdbe1b68a8a3823f
irukahaoruka <erika03301109@gmail.com>
2025-05-07 20:09:12 +0900

    firebase部分を分割

---
+++ 2PeopleTodo/2PeopleTodo/FirebaseAuthService.swift
@@ -0,0 +1,56 @@
+//
+//  FirebaseAuthService.swift
+//  PeopleTodo
+//
+//  Created by 櫻井絵理香 on 2025/05/07.
+//
+
+import FirebaseAuth
+import FirebaseCore
+
+final class FirebaseAuthService {
+    static let shared = FirebaseAuthService()
+    private let auth = Auth.auth()
+
+    private init() {
+        initializeFirebase()
+    }
+
+    private func initializeFirebase() {
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+        print("Firebase initialized")
+    }
+
+    func signInAnonymously(completion: @escaping (Result<User, Error>) -> Void) {
+        auth.signInAnonymously { result, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let user = result?.user {
+                completion(.success(user))
+            } else {
+                completion(.failure(NSError(domain: "Auth", code: 0, userInfo: [NSLocalizedDescriptionKey: "Unknown authentication error."])))
+            }
+        }
+    }
+
+    func signOut() -> Result<Void, Error> {
+        do {
+            try auth.signOut()
+            return .success(())
+        } catch {
+            return .failure(error)
+        }
+    }
+
+    func addAuthStateListener(_ callback: @escaping (User?) -> Void) {
+        auth.addStateDidChangeListener { _, user in
+            callback(user)
+        }
+    }
+
+    var currentUser: User? {
+        return auth.currentUser
+    }
+}
